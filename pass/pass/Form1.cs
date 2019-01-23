11578112addf8ea075bede30b4f64cf39f630258
Denchik1337 <h4rbonj@mail.ru>
2019-01-23 16:30:04 +0500

    test

--- pass/pass/Form1.cs
+++ pass/pass/Form1.cs
@@ -17,9 +17,8 @@ namespace pass
             InitializeComponent();
         }
 
-        public string reg(string password, string kpass)
+        public string reg(string Password, string kpass)
         {
-            string Password = textBox3.Text;
             int g = 0;
             if (Password != kpass)
             {
@@ -31,41 +30,38 @@ namespace pass
                 g++;
                 return "Длина символов пароля недостаточна или превышает лимит";
             }
-            if (Password.Contains('0') | Password.Contains('1') | Password.Contains('2') | Password.Contains('3') | Password.Contains('4') | Password.Contains('5') | Password.Contains('6') | Password.Contains('7') | Password.Contains('8') | Password.Contains('9'))
-            {
-
-            }
-            else
-            {
-                g++;
-                return "В пароле должны присутствовать цифры";
-            }
-            if (Password.Contains('*') | Password.Contains('&') | Password.Contains('{') | Password.Contains('}') | Password.Contains('|') | Password.Contains('+') | Password.Contains('.'))
-            {
-
-            }
-            else
-            {
-                g++;
-                return "Отсутсвует обязательный символ из допустимого набора: * & { } | + .";
-            }
             for (int i = 0; i < Password.Length - 2; i++)
             {
                 if (Password[i] == Password[i + 1] && Password[i + 1] == Password[i + 2])
                 {
                     g++;
                     return "Не должно присутствовать 3 повторных символов";
+                }
+            }
 
+                    if (Password.Contains('0') | Password.Contains('1') | Password.Contains('2') | Password.Contains('3') | Password.Contains('4') | Password.Contains('5') | Password.Contains('6') | Password.Contains('7') | Password.Contains('8') | Password.Contains('9'))
+                    {
+
+                    }
+                    else
+                    {
+                        g++;
+                        return "В пароле должны присутствовать цифры";
+                    }
+                if (Password.Contains('*') | Password.Contains('&') | Password.Contains('{') | Password.Contains('}') | Password.Contains('|') | Password.Contains('+') | Password.Contains('.'))
+                {
 
                 }
+                else
+                {
+                    g++;
+                    return "Отсутсвует обязательный символ из допустимого набора: * & { } | + .";
             }
             if (g == 0)
             {
                 return "Вы зарегестрированы";
             }
-                else 
-            { return "Вы не зареганы"; }
-            
+               else return "Вы не зареганы"; 
         }
         private void button1_Click(object sender, EventArgs e)
         {
--- pass/test10/UnitTest1.cs
+++ pass/test10/UnitTest1.cs
@@ -9,11 +9,120 @@ namespace test10
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void vsepravilno()
         {
             string password = "123123+";
-            string kpass = "123123";
+            string kpass = "123123+";
             string expected = "Вы зарегестрированы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+
+        [TestMethod]
+        public void netznakov()
+        {
+            string password = "123123";
+            string kpass = "123123";
+            string expected = "Вы не зареганы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void bolshe1znaka()
+        {
+            string password = "123123++";
+            string kpass = "123123++";
+            string expected = "Вы не зареганы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void povornie4isla()
+        {
+            string password = "12312333";
+            string kpass = "12312333";
+            string expected = "Вы не зареганы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void net4isel()
+        {
+            string password = "";
+            string kpass = "";
+            string expected = "Вы не зареганы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void vsepravilno1()
+        {
+            string password = "121314&";
+            string kpass = "121314&";
+            string expected = "Вы зарегестрированы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void vsepravilno2()
+        {
+            string password = "1231214155&";
+            string kpass = "1231214155&";
+            string expected = "Вы зарегестрированы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void vsepravilno3()
+        {
+            string password = "+123456";
+            string kpass = "+123456";
+            string expected = "Вы зарегестрированы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void vsepravilno4()
+        {
+            string password = "12+314";
+            string kpass = "12+314";
+            string expected = "Вы зарегестрированы";
+
+            Form1 c = new Form1();
+            string res = c.reg(password, kpass);
+
+            Assert.AreEqual(expected, res);
+        }
+        [TestMethod]
+        public void nesovpadayut()
+        {
+            string password = "12223123";
+            string kpass = "123123";
+            string expected = "Вы не зареганы";
 
             Form1 c = new Form1();
             string res = c.reg(password, kpass);
